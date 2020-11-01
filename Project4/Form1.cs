beec4757c33a1075a9671f0cf75c9b05dd08bad4
Samantha Felder <LE1937911@crc-lennox.qc.ca>
2020-11-01 18:59:04 -0500

    xz c

--- Project4/Form1.cs
+++ Project4/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Project4
 {
     public partial class Form1 : Form
-    {
+    {            
+        NpgsqlConnection dbConnection;
+
         public Form1()
         {
             InitializeComponent();
+            SetDBConnection("localhost", "postgres", "jpnul4u", "OOP");
+
+        }
+
+        /// <summary>
+        /// This method setsup a db connection to a postgreSQL db. The connection is stored in the global variable 'dbConnection'
+        /// </summary>
+        /// <param name="serverAddress">The server name or IP address</param>
+        /// <param name="username">The username having the right to manipulate the db</param>
+        /// <param name="passwd">The corresponding password of the user</param>
+        /// <param name="dbName">The db to connect to</param>
+        /// <returns></returns>
+        private void SetDBConnection(string serverAddress, string username, string passwd, string dbName)
+        {
+            //string conectionString = "Host=localhost;Username=postgres;Password=gmq715;Database=test_db";
+            string conectionString = "Host=" + serverAddress + "; Username=" + username + "; Password=" + passwd + "; Database=" + dbName + ";";
+
+            dbConnection = new NpgsqlConnection(conectionString);
         }
     }
 }
